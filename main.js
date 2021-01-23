2edb86b7e65369775933f4e11b87914f462479d7
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-01-23 10:57:09 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("CONNECTED");
+
+//SELECTORS
+//target an ID
+const div = document.getElementById("main-btn");
+console.log(div);
+
+//another, new way to do it (also works for classes)
+const divTwo = document.querySelector('#main-btn');
+console.log(divTwo);

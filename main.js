4b691d2d847f3d598b2c91f96ec997a6a99e8d74
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-01-23 11:08:39 -0600

    working with callbacks

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("CONNECTED");
+
+//CALLBACK FUNCTIONS
+// ability to pass a function a function
+
+// create two new functions
+function sayHello (name) {
+  return `Hello ${name}!`;
+}
+function sayGoodbye (name) {
+  return `Goodbye ${name} :(`;
+}
+
+// create a callback function which references the two functions above
+function dynamic(name, cb) {
+  return cb(name);
+}
+
+// log your callback function
+console.log(dynamic('Matt', sayHello)); // 'Hello Matt!'
+console.log(dynamic('Matt', sayGoodbye)); // 'Goodbye Matt :('

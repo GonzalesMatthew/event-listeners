d5321ac04a7177ee91580c90385c62ccc51fbee8
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-01-26 21:01:12 -0600

    changed for loop to forEach array method

--- main.js
+++ main.js
@@ -75,19 +75,34 @@ const printToDom = (divId, textToPrint) => {
 const pieBuilder = (taco) => {
   let domString = "";
 
-  for (let i = 0; i < taco.length; i++) {
+  // for (let i = 0; i < taco.length; i++) {
+  //   domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+  //                   <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+  //                   <div class="card-body">
+  //                   <p class="card-text">${taco[i].name}</p>
+  //                   <p class="card-text">${taco[i].ingredients}</p>
+  //                   <p class="card-text">${taco[i].bakeTemp}</p>
+  //                   <p class="card-text">${taco[i].drinkPairing}</p>
+  //                   <p class="card-text">${taco[i].iceCream}</p>
+  //                   <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+  //                   </div>
+  //                 </div>`;
+  // }
+
+
+  taco.forEach((element, i) => {
     domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
-                    <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+                    <div class="img-container" style="background-image: url('${element.imageUrl}');"></div>
                     <div class="card-body">
-                    <p class="card-text">${taco[i].name}</p>
-                    <p class="card-text">${taco[i].ingredients}</p>
-                    <p class="card-text">${taco[i].bakeTemp}</p>
-                    <p class="card-text">${taco[i].drinkPairing}</p>
-                    <p class="card-text">${taco[i].iceCream}</p>
+                    <p class="card-text">${element.name}</p>
+                    <p class="card-text">${element.ingredients}</p>
+                    <p class="card-text">${element.bakeTemp}</p>
+                    <p class="card-text">${element.drinkPairing}</p>
+                    <p class="card-text">${element.iceCream}</p>
                     <button type="button" class="btn btn-danger" id="${i}">Delete</button>
                     </div>
                   </div>`;
-  }
+  })
 
   printToDom("#pies", domString);
 };
